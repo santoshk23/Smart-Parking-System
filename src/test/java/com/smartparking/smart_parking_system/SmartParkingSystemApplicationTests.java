ad7f0238d5268b08dcac40b054df524c2e21ccdc
santosh kumar panda <santoshkumar.panda@sky.uk>
2025-06-01 13:45:13 +0530

    Initial commit

---
+++ src/test/java/com/smartparking/smart_parking_system/SmartParkingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartparking.smart_parking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
